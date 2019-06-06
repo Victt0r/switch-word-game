f953a7a189127568ac5d09680a5269791cd4d86d
Victt0r <dsnobik@gmail.com>
2019-06-06 13:37:11 +0300

    save translate

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function grabText(event) {
   if (event.key != "Enter") return
-  words = ta.value.split(' ')
+  words = ta.value.trim().split(/\s+/)
+  localStorage.words = JSON.stringify(words)
   words.forEach(spanIt)
+  translations = Array(words.length).fill("")
 
   function spanIt(word, i, arr) {
     arr[i] = "<span>"+word+"</span>"
@@ -16,3 +18,31 @@ p.onmouseleave = p.onmousemove = function (event) {
     else p.children[i].classList.remove("highlight")
   }
 }
+
+p.onclick = function (event) {
+  for (let i=0; i<p.children.length; i++) {
+    if (p.children[i] == event.target) {
+      istealth.value = ""
+      istealth.style.display = "block"
+      istealth.focus()
+      istealth.style.top = event.target.offsetTop - 19 + 'px'
+      istealth.style.left = event.target.offsetLeft + "px"
+      p.children[i].classList.add("selected")
+    }
+    else p.children[i].classList.remove("selected")
+  } 
+}
+
+istealth.onkeydown = ftch
+
+function ftch(event) {
+  if (event.key != "Enter") return
+  var i = [...p.children].indexOf(document.querySelector(".selected"))
+  translations[i] = istealth.value
+  localStorage.translations = JSON.stringify(translations)
+
+}
+
+// транслейшн переписывает пустой в локалсторадж, исправить?
+// подсветка слов и перевод
+// продолжить концепт (генерация комбинаций)
